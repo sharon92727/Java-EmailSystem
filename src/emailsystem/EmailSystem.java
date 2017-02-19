fa1926962a9d52e57cdedde08653096a5e779554
Catherine Sharon <catherine.sharon@cerner.com>
2017-02-19 16:50:33 +0530

    Java-EmailSystem

---
+++ src/emailsystem/EmailSystem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailsystem;
+
+/**
+ *
+ * @author john
+ */
+public class EmailSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
